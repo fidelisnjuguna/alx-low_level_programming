b1c2b11e417916331a0c7d9293a047c2234b911d
fidelisnjuguna <fidelnjugush@gmail.com>
2022-03-11 00:07:13 +0300

    printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ /**
+  *main - prints written quote
+  *
+  *Return: zero on success
+  *
+  */
+ int main(void)
+ {
+ 	puts("\"programming is like building a multilingual puzzle");
+ 	return (0)
+ }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - print quote using printf
+ *Description: very simple 
+ *Retun: 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0)
+}
