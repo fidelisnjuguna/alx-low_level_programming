4434c3f4b05c9dde22d929ee1f1103e050a4bc7a
fidelisnjuguna <fidelnjugush@gmail.com>
2022-03-11 00:13:12 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
- /**
-  *main - prints written quote
-  *
-  *Return: zero on success
-  *
-  */
- int main(void)
- {
- 	puts("\"programming is like building a multilingual puzzle");
- 	return (0)
- }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - print quote using printf
- *Description: very simple 
- *Retun: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
