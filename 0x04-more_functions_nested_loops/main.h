cbf1bba65d4a963274e782b9e0dd8093c04fe181
fidelisnjuguna <fidelnjugush@gmail.com>
2022-03-17 13:14:48 +0300

    supper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);int add(int, int);
+void print_times_table(int n);
+
+
+
+
+#endif
