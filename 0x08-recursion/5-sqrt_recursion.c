6bbb6151168233ca59f5add61468dd9d8a3fccba
fidelisnjuguna <fidelnjugush@gmail.com>
2022-03-30 23:38:11 +0300

    even

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,25 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural
- * square root of a number
- * @n: input number
- *
+ * _sqrt_recursion - main funct
+ * @n: int n
  * Return: int
  */
-
-int _sqrt_recursion(int n)
-{
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	if (n < 0)
-		return (-1);
-	return (getNumSqr(n, 1));
-}
+int _sqrt_recursion(int n) { return (_sqrt(n, 1)); }
 
 /**
- * getNumSqr - function that gets de num of sqroot
- *
- * @num: input number
- * @i: Counter variable
- * Return: int
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
  */
-
-int getNumSqr(int num, int i)
+int _sqrt(int n, int i)
 {
-	if (num == (i * i))
-		return (i);
-	else if (num > (i * i))
-		return (getNumSqr(num, i + 1));
-	else
-		return (-1);
+if (n < 0)
+return (-1);
+if ((i * i) > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,44 @@
 #include "main.h"
 
+int tmp_prime(int n, int i);
+
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * otherwise return 0
- * @n: input number
- *
- * Return: int
+ * divisors - number is prime?
+ * @n: integer params
+ * @m: integer params
+ * Return: boolean
+ */
+
+int divisors(int n, int m)
+{
+if (m % n == 0)
+{
+return (0);
+}
+else if (m / 2 > n)
+{
+return (divisors(n + 2, m));
+}
+else
+{
+return (1);
+}
+}
+
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ * Return: recursion
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (getNumPrim(n, 2));
-}
-
-/**
- * getNumPrim - function that gets if num is 0 or 1
- *
- * @num: input number
- * @i: Counter variable
- * Return: int
- */
-
-int getNumPrim(int num, int i)
+if ((!(n % 2) && n != 2) || n < 2)
 {
-	if (num == i)
-		return (1);
-	if (num % i == 0)
-		return (0);
-	return (getNumPrim(num, i + 1));
+return (0);
+}
+else
+{
+return (divisors(3, n));
+}
 }
