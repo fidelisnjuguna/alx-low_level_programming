3c388c7127fa80375371b4077b78c9c9e5abba7b
fidelisnjuguna <fidelnjugush@gmail.com>
2022-03-30 23:05:44 +0300

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,15 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
- char *_strstr(char *haystack, char *needle);
- void print_chessboard(char (*a)[8]);
- void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#include <stdio.h>
 
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt(int n, int i);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+ int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
